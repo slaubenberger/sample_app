8236bb62a2c2ef193bb1b3445bd27568821dc043
Stefan Laubenberger <laubenberger@gmail.com>
2012-10-14 20:55:45 +0200

    improvments at the end of chapter 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
   validates :url, format: { with: VALID_URL_REGEX, allow_nil: true, allow_blank: true }
   
